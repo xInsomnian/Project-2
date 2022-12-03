ff02d97ec2a49b55da5919cb3e25afd529f4a490
xInsomnian <98590267+xInsomnian@users.noreply.github.com>
2022-12-02 19:39:01 -0600

    Updated main file added controller added and made the view file (GUI)

---
+++ controller.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import *
+from view import *
+
+
+class Controller(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from controller import *
 
 
 def main():
 
+    app = QApplication([])
+    window = Controller()
+    window.show()
+    app.exec_()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
